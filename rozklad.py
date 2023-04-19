8bb59427a9ab3f47f4ceb499fa10f1d6637a4177
yakov <troll4875@gmail.com>
2023-04-19 16:12:44 +0300

    commit

--- rozklad.py
+++ rozklad.py
@@ -3,15 +3,10 @@ from typing import List
 
 def generate_schedule(days: int, work_days: int, rest_days: int, start_date: datetime) -> List[datetime]:
     schedule = []
-    day_count = 0
-
-    while len(schedule) < days:
-        current_date = start_date + timedelta(days=day_count)
-        if current_date.weekday() < 5 and day_count % (work_days + rest_days) < work_days:
-            schedule.append(current_date)
-        day_count += 1
-
+    for i in range(days):
+        if i % (work_days + rest_days) < work_days:
+            schedule.append(start_date)
+        start_date += timedelta(days=1)
     return schedule
-start_date = datetime(2020, 1, 30)
-schedule = generate_schedule(days=5, work_days=2, rest_days=1, start_date=start_date)
+schedule = generate_schedule(days=5, work_days=2, rest_days=1, start_date=datetime(2020, 1, 30))
 print(schedule)
